8350816bade52c66032c9a6f5d1d1ee5d32778c1
diegomanga26 <diegoalexandermanga@gmail.com>
2023-06-02 17:21:59 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# TALLER BASICO ARRAY PHP
+
+## Ejercicio #1
+
+En el ejercicio #1 tenemos la busqueda de planetas según su posición, se le indica la posición del planeta en el sistema solar para buscar el nombre del planeta. Esto tambien está incluyendo el sol como posición 1.
---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+$planetas = array(
+    "Sol"=>1,
+    "Mercurio"=>2,
+    "Venus"=>3,
+    "Tierra"=>4,
+    "Marte"=>5,
+    "Jupiter"=>6,
+    "Saturno"=>7,
+    "Urano"=>8,
+    "Neptuno"=>9
+);
+$buscador = (int)($_POST["buscador"]);
+$planetasFlip=array_flip($planetas);
+if ($buscador<=9) {
+    echo 'El planeta que buscas es: '.($planetasFlip[$buscador]);
+} else {
+    echo 'No se aceptan valores mayores a 9.';
+};
+echo "</br>";
+echo '<a href="index.html">Volver</a>';
+
+?>
